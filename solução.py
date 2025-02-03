2117c5f612d52dd52bbc1e1e30eed80d443b6cd1
Martim Gancho <martim.af.gancho@gmail.com>
2025-02-03 17:16:27 +0000

    solução.py

--- solução.py
+++ solução.py
@@ -1,29 +1,16 @@
-aluno1 = float(input("Digite a nota do aluno 1: "))
-aluno2 = float(input("Digite a nota do aluno 2: "))
-aluno3 = float(input("Digite a nota do aluno 3: "))
-aluno4 = float(input("Digite a nota do aluno 4: "))
-aluno5 = float(input("Digite a nota do aluno 5: "))
-aluno6 = float(input("Digite a nota do aluno 6: "))
-aluno7 = float(input("Digite a nota do aluno 7: "))
-aluno8 = float(input("Digite a nota do aluno 8: "))
-aluno9 = float(input("Digite a nota do aluno 9: "))
-aluno10 = float(input("Digite a nota do aluno 10: "))
-total = aluno1+aluno2+aluno3+aluno4+aluno5+aluno6+aluno7+aluno8+aluno9+aluno10
-media = total/10
-print(f"a média da nota dos alunos da turma 1 é:{media}")
-aluno11 = float(input("Digite a nota do aluno 11: "))
-aluno12 = float(input("Digite a nota do aluno 12: "))
-aluno13 = float(input("Digite a nota do aluno 13: "))
-aluno14 = float(input("Digite a nota do aluno 14: "))
-aluno15 = float(input("Digite a nota do aluno 15: "))
-aluno16 = float(input("Digite a nota do aluno 16: "))
-aluno17 = float(input("Digite a nota do aluno 17: "))
-aluno18 = float(input("Digite a nota do aluno 18: "))
-aluno19 = float(input("Digite a nota do aluno 19: "))
-aluno20 = float(input("Digite a nota do aluno 20: "))
-total2 = aluno11+aluno12+aluno13+aluno14+aluno15+aluno16+aluno17+aluno18+aluno19+aluno20
-media2 = total2/10
-totalT = media+media2
-mediaT = totalT/2
-print(f"a média da nota dos alunos da turma 2 é:{media2}")
-print(f"a média total das turmas é:{mediaT}")
+a1 = float(input("Digite a nota do aluno 1: "))
+a2 = float(input("Digite a nota do aluno 2: "))
+a3 = float(input("Digite a nota do aluno 3: "))
+a4 = float(input("Digite a nota do aluno 4: "))
+a5 = float(input("Digite a nota do aluno 5: "))
+t1 = a1+a2+a3+a4+a5
+print(f"a média da turma 1 é:{t1/5} ")
+a6 = float(input("Digite a nota do aluno 6: "))
+a7 = float(input("Digite a nota do aluno 7: "))
+a8 = float(input("Digite a nota do aluno 8: "))
+a9 = float(input("Digite a nota do aluno 9: "))
+a10 = float(input("Digite a nota do aluno 10: "))
+t2 = a6+a7+a8+a9+a10
+print(f"a média da turma 2 é:{t2/5}")
+Ttotal = t1+t2
+print(f"a média das turmas é:{Ttotal/10}")
