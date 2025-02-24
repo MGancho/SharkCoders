287160d583badc3541e04d6e26e4fd1f28926b41
Martim Gancho <martim.af.gancho@gmail.com>
2025-02-24 18:15:13 +0000

    Add files via upload

---
+++ aula 7 parte1.py
@@ -0,0 +1,9 @@
+i = int(input("Escolha um número: "))
+while i != 0:
+    if i > 0:
+        print(i)
+        i -= 1
+    else:
+        print(i)
+        i += 1
+print(i)
---
+++ aula 7 parte2.py
@@ -0,0 +1,6 @@
+i = int(input("escolhe um número: "))
+if i < 0:
+    for i in range(i,0, 1) :
+    print(i)
+elif i > 0:
+    for i in range(i,0,-1)
