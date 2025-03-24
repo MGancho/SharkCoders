ecb077c1ff13bbf0320a75815d854d5b2a9736c0
Martim Gancho <martim.af.gancho@gmail.com>
2025-03-24 18:15:25 +0000

    Add files via upload

--- aula9 parte 2.py
+++ aula9 parte 2.py
@@ -5,16 +5,20 @@ match escolha:
     case _ if escolha == 1:
         if numero % 2 == 0:
             for x in range(numero,numero2,2):
-                print('a soma de todos os números pares entre estes 2 números é de {}',format(x))
+                result = sum(range(numero,numero2,2))
+                print('a soma de todos os números pares entre estes 2 números é de {}', format(result))
         if numero % 2 != 0:
             for x in range(numero+1,numero2,2):
-                print('a soma de todos os números pares entre estes 2 números é de {}',format(x))
+                result = sum(range(numero+1,numero2,2))
+                print('a soma de todos os números pares entre estes 2 números é de {}',format(result))
     case _ if escolha == 2:
         if numero % 2 == 0:
-            for x in range(numero+1, numero2, 2):
-                print('a soma de todos os números ímpares entre estes 2 números é de {}',format(x))
+            for x in range(numero+1,numero2,2):
+                result = sum(range(numero+1,numero2,2))
+                print('a soma de todos os números ímpares entre estes 2 números é de {}',format(result))
         if numero % 2 != 0:
-            for x in range(numero + 1, numero2, 2):
-                print('a soma de todos os números ímpares entre estes 2 números é de {}', format(x))
+            for x in range(numero,numero2,2):
+                result = sum(range(numero,numero2,2))
+                print('a soma de todos os números ímpares entre estes 2 números é de {}', format(result))
     case _:
         print("tente 1 para par ou 2 para ímpar")
