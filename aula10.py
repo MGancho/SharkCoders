0930ca75df000a6414152d29b295d4b71cf00d9f
Martim Gancho <martim.af.gancho@gmail.com>
2025-03-31 18:12:38 +0100

    Add files via upload

---
+++ aula10.py
@@ -0,0 +1,20 @@
+def soma (numero):
+    total = 0
+    for x in numero:
+        total += x
+        return (total)
+def subtracao (numero):
+    total = 0
+    for x in numero:
+        total -= x
+        return (total)
+def multiplicacao (numero):
+    total = 1
+    for x in numero:
+        total *= x
+        return (total)
+def divisao (numero):
+    total = 1
+    for x in numero:
+        total /= x
+        return (total)
