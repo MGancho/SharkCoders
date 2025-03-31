f94124bda3836907ec94437378f20fd5c5d57144
Martim Gancho <martim.af.gancho@gmail.com>
2025-03-31 18:32:50 +0100

    Update aula10.py

--- aula10.py
+++ aula10.py
@@ -2,19 +2,24 @@ def soma (numero):
     total = 0
     for x in numero:
         total += x
-        return (total)
+    return (total)
 def subtracao (numero):
     total = 0
     for x in numero:
         total -= x
-        return (total)
+    return (total)
 def multiplicacao (numero):
     total = 1
     for x in numero:
         total *= x
-        return (total)
-def divisao (numero):
-    total = 1
-    for x in numero:
+    return (total)
+def divisao(numero):
+    total = numero[0]
+    for x in numero[1:]:
         total /= x
-        return (total)
+    return total
+def resto(numero):
+    total = numero[0]
+    for x in numero[1:]:
+        total %= x
+    return total
