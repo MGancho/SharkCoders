d9371eb6c9412c24fae6d41a9a0786e751c358d5
Martim Gancho <martim.af.gancho@gmail.com>
2025-06-02 18:23:34 +0100

    Add files via upload

---
+++ forsaken or forsakened.py
@@ -0,0 +1,56 @@
+from random import randint
+from time import sleep
+noob_strength = 2
+noob_intelligence = 3
+noob_stamina = 5
+
+_007n7_strength = 1
+_007n7_intelligence = 4
+_007n7_stamina = 5
+
+Shedletsky_strength = 4
+Shedletsky_intelligence = 3
+Shedletsky_stamina = 3
+
+Guest_1337_strength = 5
+Guest_1337_intelligence = 1
+Guest_1337_stamina = 4
+
+Chance_strength = randint(1,5)
+Chance_intelligence = randint(1,5)
+Chance_stamina = randint(1,5)
+
+Two_Time_strength = 5
+Two_Time_intelligence = 3
+Two_Time_stamina = 2
+
+Elliot_strength = 0
+Elliot_intelligence = 6
+Elliot_stamina = 4
+
+Dusekkar_strength = 1
+Dusekkar_intelligence = 7
+Dusekkar_stamina = 2
+
+Taph_strength = 2
+Taph_intelligence = 6
+Taph_stamina = 2
+
+Builderman_strength = 1
+Builderman_intelligence = 8
+Builderman_stamina = 1
+
+Escolha = input("START GAME? yes | no : ")
+if Escolha == "no":
+    sleep(0.5)
+    print("ok, whenever you want to start, tell me")
+elif Escolha == "yes":
+    sleep(0.5)
+    print("Let's do this")
+    sleep(0.5)
+    print("you were trapped by an entity called 'The spectre")
+    sleep(0.5)
+    print("He tortures people for fun, and he is know to be mercyless")
+    print("you and your colleagues are goin to enter the limbo")
+    sleep(0.5)
+    print("and the reward of entering and gettin out is freedom ")
